a413de29a46ac96dce8479c122701cb2eb1c0810
avnishQuovantis <91309385+avnishQuovantis@users.noreply.github.com>
2021-12-08 12:08:22 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import logo from './logo.svg';
 // import { Counter } from './features/counter/Counter';
-import './App.css';
+import './App.scss';
 import Home from './component/Home';
 import Heading from './component/Heading';
-import { BrowserRouter as Router,Route,Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 // import Basket from './component/Basket';
 import Login from './component/Login';
 import Signup from './component/Signup';
@@ -16,23 +16,25 @@ import Search from './component/Search';
 import Checkout from './component/Checkout';
 
 import ItemDetails from './component/ItemDetails';
+import User from './component/User';
 function App() {
-localStorage.setItem("currUser",null)
+  localStorage.setItem("currUser", null)
   return (
     <Router>
-    <div className="App">
-    <Heading/>
-      <Switch>
-      <Route exact path="/"><Home></Home> </Route>
-      <Route exact path="/cart"><Cart /></Route>
-      <Route path="/login"><Login/></Route>
-      <Route path="/signup"><Signup/></Route>
-      <Route path="/catagory/:name"><Catagory /></Route>
-      <Route path="/search/:search"><Search/></Route>
-      <Route path="/checkout"><Checkout/></Route>
-      <Route path="/item/:name"><ItemDetails/></Route>
-      </Switch>
-    </div>
+      <div className="App">
+        <Heading />
+        <Switch>
+          <Route exact path="/"><Home></Home> </Route>
+          <Route exact path="/cart"><Cart /></Route>
+          <Route path="/login"><Login /></Route>
+          <Route path="/signup"><Signup /></Route>
+          <Route path="/catagory/:name"><Catagory /></Route>
+          <Route path="/search/:search"><Search /></Route>
+          <Route path="/checkout"><Checkout /></Route>
+          <Route path="/item/:name"><ItemDetails /></Route>
+          <Route path="/user/:user"><User /></Route>
+        </Switch>
+      </div>
 
     </Router>
   );
